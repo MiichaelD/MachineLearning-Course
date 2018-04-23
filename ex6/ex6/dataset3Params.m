13cb5f32dec6a62ec4fbf5c489cc633df0419611
Michael Duarte <michael.duarte@hotmail.com>
2018-04-22 22:30:10 -0700

    Finises dataset4PArams.m to find the best combination of C and sigma

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -9,7 +9,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
+% Best values found ^ with a prediction error of 0.03
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +24,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+lowest_error = 100000;
 
-
-
-
-
+fprintf('Looking for optimal C and sigma values\n');
+index = 0;
+for c = values
+  for sig = values
+    fprintf('%d. - Trying C = %f and sigma = %f', ++index, c, sig);
+    model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sig));
+    error = mean(double(svmPredict(model, Xval) ~= yval));
+    if (error <= lowest_error)
+      C = c;
+      sigma = sig;
+      lowest_error = error;
+      fprintf('Best C = %f, sigma = %f, error = %f\n', c, sig, error)
+    end
+  end
+end
+printf("Best C = %f, sigma = %f\n", C, sigma);
 
 % =========================================================================
 
