ce33eaca0b4857493b18d251683772a47b420432
Michael Duarte <michael.duarte@hotmail.com>
2018-04-26 19:59:26 -0700

    Implemented computeCentroids.m

--- ex7/ex7/computeCentroids.m
+++ ex7/ex7/computeCentroids.m
@@ -27,11 +27,10 @@ centroids = zeros(K, n);
 %
 
 
-
-
-
-
-
+for i = 1:K,
+  Xi = X(idx(:,1) == i, :); % All rows with idx i
+  centroids(i, :) = mean(Xi); 
+end
 
 % =============================================================
 
